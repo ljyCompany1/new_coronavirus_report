ad7121603488a8c96cd8a5bc65f2cdc223ed2d51
ljy <89287689@qq.com>
2020-02-11 11:07:20 +0800

    1.完成投票服务器接口 2.完成1-9表格展示的接口

--- src/main/java/com/lcvc/new_coronavirus_report/web/action/backstage/QuestionnaireExcelController.java
+++ src/main/java/com/lcvc/new_coronavirus_report/web/action/backstage/QuestionnaireExcelController.java
@@ -74,20 +74,16 @@ public class QuestionnaireExcelController {
 
     //获取今天的表1-1的内容
     @GetMapping("/1-1")
-    public Map<String, Object> getTable1(){
-        Map<String, Object> map=new HashMap<String, Object>();
-        map.put(Constant.JSON_CODE, JsonCode.SUCCESS.getValue());
+    public String getTable1(){
         DailyReportTable dailyReportTable=dailyReportService.getDailyReportInTodayAndYesterDay();
         //导出表格
 
-        return map;
+        return "SUCCESS";//这里其实就是随意返回一个字符串
     }
 
     //模板，张峰-看好案例，获取今天的表1-2的内容
     @GetMapping("/1-2")
     public String getTable2(HttpServletRequest request,HttpServletResponse response){
-        Map<String, Object> map=new HashMap<String, Object>();
-        map.put(Constant.JSON_CODE, JsonCode.SUCCESS.getValue());
         QuestionnaireQuery questionnairequery=new QuestionnaireQuery();
         //questionnairequery.setQueryDate(new Date());//查找今天的表内容
         //questionnairequery.setComefromWuHan(true);//查询来自武汉的记录
@@ -102,100 +98,86 @@ public class QuestionnaireExcelController {
 
     //获取今天的表1-3的内容
     @GetMapping("/1-3")
-    public Map<String, Object> getTable3(Integer page, Integer limit){
-        Map<String, Object> map=new HashMap<String, Object>();
-        map.put(Constant.JSON_CODE, JsonCode.SUCCESS.getValue());
+    public String getTable3(Integer page, Integer limit){
         QuestionnaireQuery questionnairequery=new QuestionnaireQuery();
         questionnairequery.setQueryDate(new Date());//查找今天的表内容
         questionnairequery.setComefromHuBei(true);//查询来自湖北的记录
         List<Questionnaire> list=questionnaireService.query(questionnairequery);//获取数据记录
         //导出表格
 
-        return map;
+        return "SUCCESS";//这里其实就是随意返回一个字符串
     }
 
     //获取今天的表1-4的内容
     @GetMapping("/1-4")
-    public Map<String, Object> getTable4(Integer page, Integer limit){
-        Map<String, Object> map=new HashMap<String, Object>();
-        map.put(Constant.JSON_CODE, JsonCode.SUCCESS.getValue());
+    public String getTable4(Integer page, Integer limit){
         QuestionnaireQuery questionnairequery=new QuestionnaireQuery();
         questionnairequery.setQueryDate(new Date());//查找今天的表内容
         questionnairequery.setArriveWuHan(true);//查询去过武汉的记录
         List<Questionnaire> list=questionnaireService.query(questionnairequery);//获取数据记录
         //导出表格
 
-        return map;
+        return "SUCCESS";//这里其实就是随意返回一个字符串
     }
 
     //获取今天的表1-5的内容
     @GetMapping("/1-5")
-    public Map<String, Object> getTable5(Integer page, Integer limit){
-        Map<String, Object> map=new HashMap<String, Object>();
-        map.put(Constant.JSON_CODE, JsonCode.SUCCESS.getValue());
+    public String getTable5(Integer page, Integer limit){
         QuestionnaireQuery questionnairequery=new QuestionnaireQuery();
         questionnairequery.setQueryDate(new Date());//查找今天的表内容
         questionnairequery.setArriveHuBei(true);//查询去过湖北的记录
         List<Questionnaire> list=questionnaireService.query(questionnairequery);//获取数据记录
         //导出表格
 
-        return map;
+        return "SUCCESS";//这里其实就是随意返回一个字符串
     }
 
     //获取今天的表1-6的内容
     @GetMapping("/1-6")
-    public Map<String, Object> getTable6(Integer page, Integer limit){
-        Map<String, Object> map=new HashMap<String, Object>();
-        map.put(Constant.JSON_CODE, JsonCode.SUCCESS.getValue());
+    public String getTable6(Integer page, Integer limit){
         QuestionnaireQuery questionnairequery=new QuestionnaireQuery();
         questionnairequery.setQueryDate(new Date());//查找今天的表内容
         questionnairequery.setStayInHubei(true);//查询当前依旧停留在湖北的
         List<Questionnaire> list=questionnaireService.query(questionnairequery);//获取数据记录
         //导出表格
 
-        return map;
+        return "SUCCESS";//这里其实就是随意返回一个字符串
     }
 
     //获取今天的表1-7的内容
     @GetMapping("/1-7")
-    public Map<String, Object> getTable7(Integer page, Integer limit){
-        Map<String, Object> map=new HashMap<String, Object>();
-        map.put(Constant.JSON_CODE, JsonCode.SUCCESS.getValue());
+    public String getTable7(Integer page, Integer limit){
         QuestionnaireQuery questionnairequery=new QuestionnaireQuery();
         questionnairequery.setQueryDate(new Date());//查找今天的表内容
         questionnairequery.setTouchHuBeiPerson(true);//查询去过密切接触疫区人员的记录
         List<Questionnaire> list=questionnaireService.query(questionnairequery);//获取数据记录
         //导出表格
 
-        return map;
+        return "SUCCESS";//这里其实就是随意返回一个字符串
     }
 
     //获取今天的学生表日常记录的内容
     @GetMapping("/student")
-    public Map<String, Object> getStudentTable(Integer page, Integer limit){
-        Map<String, Object> map=new HashMap<String, Object>();
-        map.put(Constant.JSON_CODE, JsonCode.SUCCESS.getValue());
+    public String getStudentTable(Integer page, Integer limit){
         QuestionnaireQuery questionnairequery=new QuestionnaireQuery();
         questionnairequery.setQueryDate(new Date());//查找今天的表内容
         questionnairequery.setStudentQuery(true);//查询学生信息
         List<Questionnaire> list=questionnaireService.query(questionnairequery);//获取数据记录
         //导出表格
 
-        return map;
+        return "SUCCESS";//这里其实就是随意返回一个字符串
     }
 
     //获取今天的教师表日常记录的内容
     @GetMapping("/teacher")
-    public Map<String, Object> getTeacherTable(Integer page, Integer limit){
-        Map<String, Object> map=new HashMap<String, Object>();
-        map.put(Constant.JSON_CODE, JsonCode.SUCCESS.getValue());
+    public String getTeacherTable(Integer page, Integer limit){
         QuestionnaireQuery questionnairequery=new QuestionnaireQuery();
         questionnairequery.setQueryDate(new Date());//查找今天的表内容
         questionnairequery.setTeacherQuery(true);//查询教师信息
         List<Questionnaire> list=questionnaireService.query(questionnairequery);//获取数据记录
         //导出表格
 
-        return map;
+        return "SUCCESS";//这里其实就是随意返回一个字符串
     }
 
 }
